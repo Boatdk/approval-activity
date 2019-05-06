4f50698ad6488a7ffaccc78f55d92ffb5a0b9f3a
boatdk <boatz.dk@gmail.com>
2019-05-07 04:51:07 +0700

    fix data3

--- app.js
+++ app.js
@@ -151,7 +151,7 @@ app.get('/psulogin', (req, res) => {
 //////////////////////////////////LOGIN PAGES//////////////////////////////////////////////////////////////////
 app.get('/', (req, res) => {
   var data3 = session.data3 ? session.data3: req.session.data3
-  if(data){
+  if(data3){
     return res.redirect('/activity')
   }else{
     return res.render('pages/LoginV2', {
